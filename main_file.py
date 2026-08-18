6617c3d361af11d10c25c2b4c08e106eb2faccef
Arthur Sandor VK <78095669+arthursandorvk@users.noreply.github.com>
2026-08-18 09:44:21 +0800

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 import pandas as pd
+from charm.core.math.pairing import pair
 from charm.toolbox.pairinggroup import ZR, GT
 from charm.toolbox.pairinggroup import PairingGroup
 
@@ -20,26 +21,21 @@ def main():
     # SetupAA
     # WE consider three AAs
     # ----------------------------------------------------------------
-    # we register the first AA
     authority_id = 1
     authority_name = "AA1"
     attrs_1 = ['attribute1']
     puncture_maabe_obj.setupAA(authority_id, authority_name, attrs_1)
 
-    # we register the second AA
     authority_id = 2
     authority_name = "AA2"
     attrs_2 = ['attribute2']
     puncture_maabe_obj.setupAA(authority_id, authority_name, attrs_2)
 
-    # we register the third AA
     authority_id = 3
     authority_name = "AA3"
     attrs_3 = ['attribute3', 'attribute4']
     puncture_maabe_obj.setupAA(authority_id, authority_name, attrs_3)
     # ----------------------------------------------------------------
-
-
 
     # set of user attributes
     attrs = {}
@@ -48,169 +44,132 @@ def main():
     attrs['attribute3'] = 'attribute3$THREE'
     attrs['attribute4'] = 'attribute4$FOUR'
 
-    # we register a user
-    user = {}
+    # we register a user with anonymity level
     user_gid = 'gid'
-    user = puncture_maabe_obj.regUser(gid=user_gid, attributes=attrs, anonymity_level= 10)
+    anonymity_level = 10
+    user = puncture_maabe_obj.regUser(gid=user_gid, attributes=attrs, anonymity_level=anonymity_level)
 
-    #
-    access_policy = f"(({attrs['attribute4']} or {attrs['attribute3']}) and ({attrs['attribute3']} or {attrs['attribute1']}))" #f"(({four_hidden} or {three_hidden}) and ({three_hidden} or {one_hidden}))"
+    access_policy = f"(({attrs['attribute4']} or {attrs['attribute3']}) and ({attrs['attribute3']} or {attrs['attribute1']}))"
+    access_policy_schema = f"(({attrs['attribute4'].split('$',1)[0]} or {attrs['attribute3'].split('$',1)[0]}) and ({attrs['attribute3'].split('$',1)[0]} or {attrs['attribute1'].split('$',1)[0]}))"
 
-    # we need to hide the attribute values
-    access_policy = f"(({attrs['attribute4']} or {attrs['attribute3']}) and ({attrs['attribute3']} or {attrs['attribute1']}))"  # f"(({four_hidden} or {three_hidden}) and ({three_hidden} or {one_hidden}))"
-    access_policy_schema = f"(({attrs['attribute4'].split('$',1)[0]} or {attrs['attribute3'].split('$',1)[0]}) and ({attrs['attribute3'].split('$',1)[0]} or {attrs['attribute1'].split('$',1)[0]}))"  # f"(({four_hidden} or {three_hidden}) and ({three_hidden} or {one_hidden}))"
-    #
-
-    '''
-    how to save / restore a group element
-    print(f"before serialization, this is D_j value:  {D_j} \n\n")
-
-    my_test_TKDU_serialize = self.crs['group'].serialize(D_j, compression=False)
-    print(f" my test D_j serialization is {my_test_TKDU_serialize} \n\n")
-
-    my_TKDU_Restoration = self.crs['group'].deserialize(my_test_TKDU_serialize)
-    print(f" my test D_j restauration is {my_TKDU_Restoration} \n\n")
-    #
-    '''
-    # hidden_attrs = {}
-    # hidden_attrs['attribute1'] = f"attribute1${(puncture_maabe_obj.crs['group'].serialize(puncture_maabe_obj.hide_by_DO(mk, attribute_value='ONE', epsilon='epsilon'), compression=False)).decode('utf-8')}"
-    # hidden_attrs['attribute2'] = f"attribute2${(puncture_maabe_obj.crs['group'].serialize(puncture_maabe_obj.hide_by_DO(mk, attribute_value='TWO', epsilon='epsilon'), compression=False)).decode('utf-8')}"
-    # hidden_attrs['attribute3'] = f"attribute3${(puncture_maabe_obj.crs['group'].serialize(puncture_maabe_obj.hide_by_DO(mk, attribute_value='THREE', epsilon='epsilon'), compression=False)).decode('utf-8')}"
-    # hidden_attrs['attribute4'] = f"attribute4${(puncture_maabe_obj.crs['group'].serialize(puncture_maabe_obj.hide_by_DO(mk, attribute_value='FOUR', epsilon='epsilon'), compression=False)).decode('utf-8')}"
     hiden_attr_1 = puncture_maabe_obj.hide_by_DO(mk, attribute_value='ONE', epsilon='epsilon')
     hiden_attr_2 = puncture_maabe_obj.hide_by_DO(mk, attribute_value='TWO', epsilon='epsilon')
     hiden_attr_3 = puncture_maabe_obj.hide_by_DO(mk, attribute_value='THREE', epsilon='epsilon')
     hiden_attr_4 = puncture_maabe_obj.hide_by_DO(mk, attribute_value='FOUR', epsilon='epsilon')
 
-    # print("********* list attribute values hidden by DO (group elements) ***** \n")
-    # print(f"attribute 1: {hiden_attr_1}\n")
-    # print(f"attribute 2: {hiden_attr_2}\n")
-    # print(f"attribute 3: {hiden_attr_3}\n")
-    # print(f"attribute 4: {hiden_attr_4}\n")
-
-    # hidden_attrs = {}
-    # hidden_attrs['attribute1'] = f"attribute1${(puncture_maabe_obj.crs['group'].serialize(hiden_attr_1, compression=False)).hex()}"
-    # hidden_attrs['attribute2'] = f"attribute2${(puncture_maabe_obj.crs['group'].serialize(hiden_attr_2, compression=False)).hex()}"
-    # hidden_attrs['attribute3'] = f"attribute3${(puncture_maabe_obj.crs['group'].serialize(hiden_attr_3, compression=False)).hex()}"
-    # hidden_attrs['attribute4'] = f"attribute4${(puncture_maabe_obj.crs['group'].serialize(hiden_attr_4, compression=False)).hex()}"
-    #
     hidden_attrs = {}
     hidden_attrs['attribute1'] = puncture_maabe_obj.crs['group'].serialize(hiden_attr_1, compression=False).hex().upper()
     hidden_attrs['attribute2'] = puncture_maabe_obj.crs['group'].serialize(hiden_attr_2, compression=False).hex().upper()
     hidden_attrs['attribute3'] = puncture_maabe_obj.crs['group'].serialize(hiden_attr_3, compression=False).hex().upper()
     hidden_attrs['attribute4'] = puncture_maabe_obj.crs['group'].serialize(hiden_attr_4, compression=False).hex().upper()
 
+    hidden_access_policy = f"(({hidden_attrs['attribute4']} or {hidden_attrs['attribute3']}) and ({hidden_attrs['attribute3']} or {hidden_attrs['attribute1']}))"
 
-
-    # hidden_attrs_policy = {}
-    # hidden_attrs_policy['attribute1'] = f"attribute1${puncture_maabe_obj.hide_by_DO(mk, attribute_value='ONE', epsilon='epsilon')}"
-    # hidden_attrs_policy['attribute2'] = f"attribute2${puncture_maabe_obj.hide_by_DO(mk, attribute_value='TWO', epsilon='epsilon')}"
-    # hidden_attrs_policy['attribute3'] = f"attribute3${puncture_maabe_obj.hide_by_DO(mk, attribute_value='THREE', epsilon='epsilon')}"
-    # hidden_attrs_policy['attribute4'] = f"attribute4${puncture_maabe_obj.hide_by_DO(mk, attribute_value='FOUR', epsilon='epsilon')}"
-
-
-    # only attribute values
-    # hidden_attrs = {}
-    # hidden_attrs['attribute1'] = (puncture_maabe_obj.hide_by_DO(mk, attribute_value='ONE', epsilon='epsilon'))
-    # hidden_attrs['attribute2'] = (puncture_maabe_obj.hide_by_DO(mk, attribute_value='TWO', epsilon='epsilon'))
-    # hidden_attrs['attribute3'] = (puncture_maabe_obj.hide_by_DO(mk, attribute_value='THREE', epsilon='epsilon'))
-    # hidden_attrs['attribute4'] = (puncture_maabe_obj.hide_by_DO(mk, attribute_value='FOUR', epsilon='epsilon'))
-
-    # hidden_attrs = {}
-    # hidden_attrs['attribute1'] = map(str, 'attribute1$'+{puncture_maabe_obj.hide_by_DO(mk, attribute_value='ONE', epsilon='epsilon')})
-    # hidden_attrs['attribute2'] = map(str, 'attribute2$'+{puncture_maabe_obj.hide_by_DO(mk, attribute_value='TWO', epsilon='epsilon')})
-    # hidden_attrs['attribute3'] = map(str, 'attribute3$'+{puncture_maabe_obj.hide_by_DO(mk, attribute_value='THREE', epsilon='epsilon')})
-    # hidden_attrs['attribute4'] = map(str, 'attribute4$'+{puncture_maabe_obj.hide_by_DO(mk, attribute_value='FOUR', epsilon='epsilon')})
-
-    # hidden_attrs = {}
-    # hidden_attr_1 = f"attribute1${puncture_maabe_obj.hide_by_DO(mk, attribute_value='ONE', epsilon='epsilon')}"
-    # hidden_attr_2 = f"attribute2${puncture_maabe_obj.hide_by_DO(mk, attribute_value='TWO', epsilon='epsilon')}"
-    # hidden_attr_3 = f"attribute3${puncture_maabe_obj.hide_by_DO(mk, attribute_value='THREE', epsilon='epsilon')}"
-    # hidden_attr_4 = f"attribute4${puncture_maabe_obj.hide_by_DO(mk, attribute_value='FOUR', epsilon='epsilon')}"
-
-    # hidden_access_policy = f"(({(hidden_attrs['attribute4'].split('$',1)[1])} or {hidden_attrs['attribute3'].split('$',1)[1]}) and ({hidden_attrs['attribute3'].split('$',1)[1]} or {hidden_attrs['attribute1'].split('$',1)[1]}))"  # f"(({four_hidden} or {three_hidden}) and ({three_hidden} or {one_hidden}))"
-    # hidden_access_policy = f"(({hidden_attrs['attribute4'].split('$',1)[1]} or {hidden_attrs['attribute3'].split('$',1)[1]}) and ({hidden_attrs['attribute3'].split('$',1)[1]} or {hidden_attrs['attribute1'].split('$',1)[1]}))"  # f"(({four_hidden} or {three_hidden}) and ({three_hidden} or {one_hidden}))"
-    # hidden_access_policy = f"(({hidden_attrs_policy['attribute4']} or {hidden_attrs_policy['attribute3']}) and ({hidden_attrs_policy['attribute3']} or {hidden_attrs_policy['attribute1']}))"  # f"(({four_hidden} or {three_hidden}) and ({three_hidden} or {one_hidden})'
-    hidden_access_policy = f"(({hidden_attrs['attribute4']} or {hidden_attrs['attribute3']}) and ({hidden_attrs['attribute3']} or {hidden_attrs['attribute1']}))"  # f"(({four_hidden} or {three_hidden}) and ({three_hidden} or {one_hidden})'
-    #
-    # hidden_access_policy = f'({hidden_attr_4} or {hidden_attr_3}) and ({hidden_attr_3} or {hidden_attr_1})'  # f"(({four_hidden} or {three_hidden}) and ({three_hidden} or {one_hidden})'
-
-    print(f" the value of the ****** hidden policy ******* is  {hidden_access_policy}\n\n\n")
-    # print("this is the access policy schema ", access_policy_schema)
+    print(f" the value of the ******* hidden policy ******** is {hidden_access_policy}\n\n\n")
 
     if debug:
         print("Unhidden Attributes =>", attrs, "\n")
         print("Policy =>", access_policy, "\n")
 
-
-    #1- DO generate the DO Key
+    # 1- DO generate the DO Key
     do_key = puncture_maabe_obj.keygen1(mk, puncture_maabe_obj.users[user_gid].get(user_gid), prv_DO, epsilon_value)
-    #
+
     # 2- DU hides the set of attributes originally in the plain form
-    # print(" the set of attributes for the current user before hiding them ", puncture_maabe_obj.users[user_gid].get('attributes'))
-    puncture_maabe_obj.users[user_gid]['attributes'] = puncture_maabe_obj.hide_attr(puncture_maabe_obj.users[user_gid].get('gid'), do_key, puncture_maabe_obj.users[user_gid].get('attributes'))
-    # print(" the set of hidden attributes for the current user ", puncture_maabe_obj.users[user_gid].get('attributes'))
-    #
+    puncture_maabe_obj.users[user_gid]['attributes'] = puncture_maabe_obj.hide_attr(
+        puncture_maabe_obj.users[user_gid].get('gid'), do_key, puncture_maabe_obj.users[user_gid].get('attributes'))
+
     # AAs compute Keygen2
-    # WE consider three AAs
-    # set of attributes
-    aa_key_1 = puncture_maabe_obj.keygen2(AA_ID=1, gid=puncture_maabe_obj.users[user_gid].get('gid'), S_DU=puncture_maabe_obj.users[user_gid]['attributes'])
+    aa_key_1 = puncture_maabe_obj.keygen2(AA_ID=1, gid=puncture_maabe_obj.users[user_gid].get('gid'),
+                                          S_DU=puncture_maabe_obj.users[user_gid]['attributes'])
     print(f" the gid is {puncture_maabe_obj.users[user_gid].get('gid')} and the set of attributes is {puncture_maabe_obj.users[user_gid].get('attributes')} \n\n")
 
-    # set of attributes
-    aa_key_2 = puncture_maabe_obj.keygen2(AA_ID=2, gid=puncture_maabe_obj.users[user_gid].get('gid'), S_DU=puncture_maabe_obj.users[user_gid]['attributes'])
+    aa_key_2 = puncture_maabe_obj.keygen2(AA_ID=2, gid=puncture_maabe_obj.users[user_gid].get('gid'),
+                                          S_DU=puncture_maabe_obj.users[user_gid]['attributes'])
 
-    # set of attributes
-    aa_key_3 = puncture_maabe_obj.keygen2(AA_ID=3, gid=puncture_maabe_obj.users[user_gid].get('gid'), S_DU=puncture_maabe_obj.users[user_gid]['attributes'])
-    #
+    aa_key_3 = puncture_maabe_obj.keygen2(AA_ID=3, gid=puncture_maabe_obj.users[user_gid].get('gid'),
+                                          S_DU=puncture_maabe_obj.users[user_gid]['attributes'])
+
     input_aa_key = list()
     input_aa_key.append(aa_key_1)
     input_aa_key.append(aa_key_2)
     input_aa_key.append(aa_key_3)
-    # input_aa_key[0]= aa_key_1
-    # input_aa_key[1] = aa_key_2
-    # input_aa_key[2] = aa_key_3
 
     if debug:
         print(f"Hidden Attributes => \n {puncture_maabe_obj.users[user_gid]['attributes']} \n")
 
     tk_key = puncture_maabe_obj.keygen3(input_aa_key, user_gid)
-    #
     DU_key, DU_hkey = puncture_maabe_obj.keygen4(do_key, tk_key)
-    #
 
-    # ***********************Encryption************
+    # ******************Encryption******************
     rand_msg = puncture_maabe_obj.crs['group'].random(GT)
-    # print(f' the original  message is {rand_msg} \n')
-    #
 
     CT = puncture_maabe_obj.encrypt(pk, mk, rand_msg, access_policy, prv_DO, epsilon_value, hidden_access_policy)
-    #
+
+
+    if puncturing is True:
+        # we perform puncture operations
+            if single_puncture:
+                print("\n=== SINGLE TAG PUNCTURING ===\n")
+                tag_to_puncture = "revoked_user_001"
+                DU_key, DU_hkey, T_prime, desc_T_prime = puncture_maabe_obj.DU_single_puncture(
+                    DU_key, DU_hkey, tag_to_puncture, user_gid, anonymity_level)
+                print(f"Punctured tag: {tag_to_puncture}")
+                print(f"Anonymous set size: {len(T_prime)} (1 real + {anonymity_level - 1} dummy)")
+            else:
+                print("\n=== BATCH TAG PUNCTURING ===\n")
+                tags_to_puncture = {"revoked_user_001", "revoked_user_002", "revoked_user_003"}
+                DU_key, DU_hkey, T_prime, desc_T_prime = puncture_maabe_obj.DU_batch_puncture(
+                DU_key, DU_hkey, tags_to_puncture, user_gid, anonymity_level)
+                print(f"Punctured tags: {tags_to_puncture}")
+                print(f"Anonymous set size: {len(T_prime)} ({len(tags_to_puncture)} real + {anonymity_level - len(tags_to_puncture)} dummy)")
+
+            # DU outsources remaining puncturing to CSP
+            DU_hkey, sig_ACC = puncture_maabe_obj.csp_puncture(
+                CT, DU_hkey, T_prime, desc_T_prime, user_gid)
+
+            # Store CSP attestation data in CT for verification
+            CT['sig_ACC'] = sig_ACC
+
+
+            print(f"Accumulator value after CSP puncturing: {CT['ACC'].acc_get_value()}")
+            print(f"Accumulator size: {CT['ACC'].acc_get_size()}")
+            print(f"CSP attestation signature generated.\n")
+
+    # ******************Transform******************
     TC, I = puncture_maabe_obj.transform(CT=CT, DU_hkey=DU_hkey, gid=puncture_maabe_obj.users[user_gid].get('gid'))
 
-    rec_msg = puncture_maabe_obj.Decrypt(CT, TC, I, do_key, pub_DO, puncture_maabe_obj.user_secret_product)
+    # ******************Decrypt******************
+    rec_msg = puncture_maabe_obj.Decrypt(CT, TC, I, DU_key, pub_DO)
 
-    # if debug: print("\n\nCiphertext...\n")
-    # groupObj.debug(ct)
+    if (rand_msg != rec_msg):
+        #
+        RED = '\033[91m'
+        RESET = '\033[0m'
+        print(f"{RED}FAILED Decryption: message is incorrect {RESET}\n\n")
+        # assert rand_msg == rec_msg, "FAILED Decryption: message is incorrect"
+    else:
+        if debug is True:
+            print(f" Original msg: {rand_msg} \n")
+            print(f" msg recovered: {rec_msg} \n")
+            print("Successful Decryption!!!")
+        else:
+            print("Decryption successful - message recovered correctly.")
 
-    assert rand_msg == rec_msg, "FAILED Decryption: message is incorrect"
-
-    if debug is True:
-        print(f" Original msg: {rand_msg} \n")
-        print(f" msg recovered: {rec_msg} \n")
-    print("Successful Decryption!!!")
-
-    myset = defaultdict(dict)
-    myset['a']=(('a',puncture_maabe_obj.crs['group'].serialize(puncture_maabe_obj.crs['group'].hash('a',ZR), compression=False).hex().upper()))
-
-    myset_list = list()
-    myset_list.append(myset)
-
-    print(f"myset: {myset['a']}")
+    if puncturing is True:
+        # we can verify accumulator attestation signature
+        acc_val = CT['ACC'].acc_get_value()
+        left_pair = pair(puncture_maabe_obj.crs['g1'], CT['sig_ACC'])
+        right_pair = pair(puncture_maabe_obj.crs['g1'] ** puncture_maabe_obj.crs['group'].hash(str(acc_val), ZR), puncture_maabe_obj.public_attestation_key)
+        assert left_pair == right_pair, "Accumulator attestation signature verification failed!"
+        print("Accumulator attestation signature verified successfully.")
 
 
 if __name__ == "__main__":
     debug = False
+    # ******************PUNCTURING PHASE******************
+    # whether to puncture or not
+    puncturing = False
+    # whether to perform batch or single puncture
+    single_puncture = True
     #
-    single_puncture = False
     main()
