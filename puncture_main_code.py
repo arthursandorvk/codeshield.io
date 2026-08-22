283c87ecf7578a8a3f996192b84b88943db70f01
Arthur Sandor VK <78095669+arthursandorvk@users.noreply.github.com>
2026-08-23 00:21:40 +0800

    Update puncture_main_code.py

--- puncture_main_code.py
+++ puncture_main_code.py
@@ -23,7 +23,7 @@ import string
 import uuid
 
 from charm.schemes.abenc.abenc_bsw07 import CPabe_BSW07
-from charm.toolbox.pairinggroup import PairingGroup, ZR, G1, G2, GT, pair
+from charm.toolbox.pairinggroup import PairingGroup, ZR, G1, GT, pair
 from charm.toolbox.policytree import PolicyParser
 from charm.toolbox.secretutil import SecretUtil
 from charm.toolbox.ABEnc import ABEnc, Input, Output
@@ -33,9 +33,7 @@ from typing import List
 
 from collections import defaultdict
 
-
-
-from PolicyParser_Modified import PolicyParserModified
+#from PolicyParser_Modified import PolicyParserModified
 from accumulator import accumulator
 
 debug = True
@@ -45,7 +43,7 @@ crs_t = {'g': G1, 'e_gg': GT}
 
 # type annotations
 pp_t = {'h': G1, 'f': G1, 'e_gg_alpha': GT}
-msk_t = {'beta': ZR, 'g_alpha': G2}
+msk_t = {'beta': ZR, 'g_alpha': G1}
 #
 
 """
@@ -91,7 +89,7 @@ class P_MACP_ABE(ABEnc):
         # the transformed ciphertext
         self.TC = None
 
-        #-------------------------------------------------
+        # -------------------------------------------------
         # Generate CSP attestation key pair (BLS-style)
         #
         # the secret attestation key from the transparent TEE
@@ -99,6 +97,7 @@ class P_MACP_ABE(ABEnc):
         #
         # the public attestation key from the transparent TEE
         self.public_attestation_key = self.crs['g'] ** self.secret_attestation_key
+
     # --------------------------------------------------------------------------------------------------------
 
     def regUser(self, gid: str, attributes: list, anonymity_level: int):
@@ -135,8 +134,7 @@ class P_MACP_ABE(ABEnc):
             }
         return self.authorities[authorityid]
 
-
-    def setup_(self):
+    def setup(self):
         a0 = self.crs['group'].random(ZR)
         b0 = self.crs['group'].random(ZR)
         prv_DO = a0 * b0
@@ -151,12 +149,11 @@ class P_MACP_ABE(ABEnc):
 
         pp = {'h': h, 'e_gg_alpha': e_gg_alpha}
         msk = {'beta': beta, 'g_alpha': g_alpha}
+        #
         return prv_DO, pub_DO, pp, msk
-
 
     def keygen(self, pp, msk, gid):
         pass
-
 
     def keygen1(self, msk, gid, prv_DO, epsilon: str):
         gamma = self.crs['group'].hash(str(gid), ZR)
@@ -164,7 +161,7 @@ class P_MACP_ABE(ABEnc):
         self.random_user_exponent = r
 
         sk_DO = (msk['g_alpha'] ** (1 / msk['beta'])) * (self.users[gid]['pub_DU'] ** (1 / (msk['beta']))) * (
-            self.crs['g'] ** (r * (1 / (msk['beta']))))
+                self.crs['g'] ** (r * (1 / (msk['beta']))))
 
         hk_DO_1 = (self.crs['g'] ** gamma) * (self.crs['g'] ** r) * (self.users[gid]['pub_DU'])
         hk_DO_2 = self.crs['g'] ** (self.crs['group'].hash(str(epsilon), ZR) * gamma * (1 / msk['beta']))
@@ -178,21 +175,20 @@ class P_MACP_ABE(ABEnc):
                   'desc_T': desc_T}
         return DO_key
 
-
     def hide_attr(self, gid, DO_key, S_DU: dict) -> {}:
         S_DU_hidden = {}
         gamma = self.crs['group'].hash(str(gid), ZR)
         for att_name in S_DU.keys():
             hidden_att_value = (self.crs['g'] ** (
-                self.crs['group'].hash(S_DU.get(att_name).split("$", 1)[1], ZR) * gamma)) * DO_key[
-                'hk_2']
+                    self.crs['group'].hash(S_DU.get(att_name).split("$", 1)[1], ZR) * gamma)) * DO_key[
+                                   'hk_2']
             S_DU_hidden[att_name] = (att_name, hidden_att_value)
         return S_DU_hidden
 
     # this is called by DO to hide attributes prior to design the access policy - not a function on its own
     def hide_by_DO(self, msk, attribute_value: str, epsilon: str):
         return (self.crs['g'] ** (self.crs['group'].hash(str(attribute_value), ZR))) * (
-            self.crs['g'] ** ((self.crs['group'].hash(str(epsilon), ZR)) * (1 / (msk['beta']))))
+                self.crs['g'] ** ((self.crs['group'].hash(str(epsilon), ZR)) * (1 / (msk['beta']))))
 
     def keygen2(self, AA_ID, gid, S_DU: dict):
         gamma_du = self.crs['group'].hash(str(gid), ZR)
@@ -217,7 +213,6 @@ class P_MACP_ABE(ABEnc):
             AA_key[(att_name, attr_value)] = {'D_j': D_j, 'T_j': T_j, 'attr': att_name}
         return AA_key
 
-
     def keygen3(self, aa_key_list: list, gid):
         TK_DU = {}
         items = list()
@@ -234,7 +229,6 @@ class P_MACP_ABE(ABEnc):
         # print(f" \n\n****************************this is the structure of TK_DU {TK_DU}\n\n")
         return TK_DU
 
-
     def keygen4(self, DO_key, TK_DU):
         DU_key = {'sk': DO_key['sk'], 'hk': TK_DU}
         DU_hkey = {'hk_1': DO_key['hk_1'], 'hk_2': DO_key['hk_2'], 'hk_3': DO_key['hk_3'], 'T': DO_key['T'],
@@ -242,13 +236,12 @@ class P_MACP_ABE(ABEnc):
                    'items': TK_DU['items']}
         return DU_key, DU_hkey
 
-
     def DU_single_puncture(self, DU_key, DU_hkey, tag_name: str, gid, anonym_level: int):
         T_prime = set()
 
         r_real_tag = self.crs['group'].random(ZR)
         real_tag_component = self.crs['g'] ** (
-                    r_real_tag * self.crs['group'].hash(str(tag_name), ZR) * self.crs['group'].hash(str(gid), ZR))
+                r_real_tag * self.crs['group'].hash(str(tag_name), ZR) * self.crs['group'].hash(str(gid), ZR))
 
         T_prime.add(tag_name)
         desc_T_prime = defaultdict(dict)
@@ -267,8 +260,8 @@ class P_MACP_ABE(ABEnc):
             dummy_tag_name = f'{dummy_tag}'
             r_dummy_tag = self.crs['group'].random(ZR)
             dummy_tag_component = self.crs['g'] ** (
-                        self.crs['group'].hash(str(dummy_tag_name), ZR) * r_dummy_tag * self.crs['group'].hash(
-                    str(gid), ZR) / sum_dummy_tag_list_elements)
+                    self.crs['group'].hash(str(dummy_tag_name), ZR) * r_dummy_tag * self.crs['group'].hash(
+                str(gid), ZR) / sum_dummy_tag_list_elements)
 
             dummy_tag_dict[dummy_tag_name] = r_dummy_tag
             T_prime.add(dummy_tag_name)
@@ -285,8 +278,10 @@ class P_MACP_ABE(ABEnc):
             for dummy_tag in dummy_tag_dict.keys():
                 r_dummy = dummy_tag_dict[dummy_tag]
                 update_term_Dj = update_term_Dj * (att_value ** ((
-                            self.crs['group'].hash(str(dummy_tag), ZR) * r_dummy * self.crs['group'].hash(str(gid),
-                                                                                                        ZR)) / sum_dummy_tag_list_elements))
+                                                                         self.crs['group'].hash(str(dummy_tag),
+                                                                                                ZR) * r_dummy *
+                                                                         self.crs['group'].hash(str(gid),
+                                                                                                ZR)) / sum_dummy_tag_list_elements))
             DU_hkey['TK_DU'][index_value_hex]['D_j'] = DU_hkey['TK_DU'][index_value_hex]['D_j'] * update_term_Dj
 
         product_dummy_tag_list_elements = math.prod(dummy_tag_list_apply_hash_per_element)
@@ -297,7 +292,6 @@ class P_MACP_ABE(ABEnc):
 
         return DU_key, DU_hkey, T_prime, desc_T_prime
 
-
     def DU_batch_puncture(self, DU_key, DU_hkey, tag_name_list: set, gid: str, anonym_level: int):
         T_prime = set()
         desc_T_prime = defaultdict(dict)
@@ -306,8 +300,8 @@ class P_MACP_ABE(ABEnc):
         for real_tag_name in tag_name_list:
             real_tag_element = self.crs['group'].random(ZR)
             real_tag_component = self.crs['g'] ** (
-                        self.crs['group'].hash(str(real_tag_name), ZR) * real_tag_element * self.crs['group'].hash(
-                    str(gid), ZR))
+                    self.crs['group'].hash(str(real_tag_name), ZR) * real_tag_element * self.crs['group'].hash(
+                str(gid), ZR))
             T_prime.add(real_tag_name)
             desc_T_prime[real_tag_name] = real_tag_component
             DU_hkey['T'].add(real_tag_name)
@@ -322,8 +316,8 @@ class P_MACP_ABE(ABEnc):
             dummy_tag_name = f'{dummy_tag}'
             r_dummy_tag = self.crs['group'].random(ZR)
             dummy_tag_component = self.crs['g'] ** (
-                        self.crs['group'].hash(str(dummy_tag_name), ZR) * r_dummy_tag * self.crs['group'].hash(
-                    str(gid), ZR) / sum_dummy_tag_list_elements)
+                    self.crs['group'].hash(str(dummy_tag_name), ZR) * r_dummy_tag * self.crs['group'].hash(
+                str(gid), ZR) / sum_dummy_tag_list_elements)
 
             dummy_tag_dict[dummy_tag_name] = r_dummy_tag
             T_prime.add(dummy_tag_name)
@@ -340,8 +334,10 @@ class P_MACP_ABE(ABEnc):
             for dummy_tag in dummy_tag_dict.keys():
                 r_dummy = dummy_tag_dict[dummy_tag]
                 update_term_Dj = update_term_Dj * (att_value ** ((
-                            self.crs['group'].hash(str(dummy_tag), ZR) * r_dummy * self.crs['group'].hash(str(gid),
-                                                                                                        ZR)) / sum_dummy_tag_list_elements))
+                                                                         self.crs['group'].hash(str(dummy_tag),
+                                                                                                ZR) * r_dummy *
+                                                                         self.crs['group'].hash(str(gid),
+                                                                                                ZR)) / sum_dummy_tag_list_elements))
             DU_hkey['TK_DU'][index_value_hex]['D_j'] = DU_hkey['TK_DU'][index_value_hex]['D_j'] * update_term_Dj
 
         product_dummy_tag_list_elements = math.prod(dummy_tag_list_apply_hash_per_element)
@@ -351,7 +347,6 @@ class P_MACP_ABE(ABEnc):
         DU_hkey['hk_2'] = DU_hkey['hk_2'] ** (product_dummy_tag_list_elements)
 
         return DU_key, DU_hkey, T_prime, desc_T_prime
-
 
     # ask for attestation secret key
     def csp_puncture(self, CT, DU_hkey, T_prime: set, desc_T_prime: defaultdict(dict), gid: str):
@@ -467,7 +462,7 @@ class P_MACP_ABE(ABEnc):
         # print(f" the computed value of TC is: {TC} \n")
 
         TC = (pair(self.crs['g'], self.crs['g']) ** (
-            (self.random_user_exponent + self.crs['group'].hash(str(gid), ZR)) * self.shared_secret)) * (
+                (self.random_user_exponent + self.crs['group'].hash(str(gid), ZR)) * self.shared_secret)) * (
                      (pair(self.crs['g'], self.crs['g'])) ** (self.users[gid]['prv_DU'] * self.shared_secret))
 
         self.TC = TC
@@ -487,7 +482,7 @@ class P_MACP_ABE(ABEnc):
         W_prime = self.crs['g'] ** (self.crs['group'].hash(str(M_prime), ZR))
 
         if pair(self.crs['g'], CT['sig_W']) == pair(self.crs['g'] ** self.crs['group'].hash(str(W_prime), ZR),
-                                                     pub_DO):
+                                                    pub_DO):
             return M_prime
         else:
             return None
